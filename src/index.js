fa85938e73c8fcdcdce0bb49e40a72cf85756b9c
TatyanaShu <tashu529@gmail.com>
2021-03-13 18:07:33 +0300

    fix: fix mistakes code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
-matrix.flat().sort();
+let answer=[]
 module.exports = function towelSort (matrix) {
-  return [];
+    if (arguments.length==0|| matrix.length==0){ return []}
+   else {
+    //    answer=matrix.flat().sort(function(a,b){ return a - b;}
+    for (i=0; i<matrix.length; i++) {
+        if (i%2>0) {
+            matrix[i].sort(function(a,b){ return b - a;})
+        }
+    }
+    answer=matrix.flat();
+
+  return answer}
 }
